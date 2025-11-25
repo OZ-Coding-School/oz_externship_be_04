801f2fd2b8f2602f48035994d6399cea4eabf6eb
s4ngmin-9 <s4ngmin9@naver.com>
2025-11-25 19:20:13 +0900

    🎨 style: black, isort 코드 포매터 실행

--- apps/chat/models/__init__.py
+++ apps/chat/models/__init__.py
@@ -2,6 +2,6 @@ from apps.chat.models.chat_message import ChatMessage
 from apps.chat.models.last_read_message import LastReadMessage
 
 __all__ = [
-    'ChatMessage',
-    'LastReadMessage',
+    "ChatMessage",
+    "LastReadMessage",
 ]
