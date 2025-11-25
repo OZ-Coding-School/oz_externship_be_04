e63fb85e1b7433cff03d4b1dba28568427e8e22d
daebagi <daeburm0115@naver.com>
2025-11-25 18:41:28 +0900

    🐛 fix: user rebase 후 모델 임포트, def 타입힌트 추가

--- apps/lectures/models/user_prefer_category.py
+++ apps/lectures/models/user_prefer_category.py
@@ -1,11 +1,12 @@
 from django.db import models
 
 from apps.lectures.models.category import Category
+from apps.users.models import Users
 
 
 class UserPreferCategory(models.Model):
     pk = models.CompositePrimaryKey("user_id", "category_id", help_text="복합 pk키")
-    user = models.ForeignKey(User, on_delete=models.CASCADE, help_text="유저 id")
+    user = models.ForeignKey(Users, on_delete=models.CASCADE, help_text="유저 id")
     category = models.ForeignKey(Category, on_delete=models.CASCADE, help_text="카테고리 id")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -14,5 +15,5 @@ class UserPreferCategory(models.Model):
         ordering = ["pk"]
         db_table = "user_prefer_categories"
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"[{self.user}] {self.category}"
