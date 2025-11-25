d74aa8d818cb05af53d215d240ebf971dbfbf2c0
daebagi <daeburm0115@naver.com>
2025-11-25 18:07:29 +0900

    ✨ feat: 카테고리 모델 작성

---
+++ apps/lectures/models/category.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True, help_text="카테고리명")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["name"]
+        db_table = "categories"
+
+    def __str__(self):
+        return self.name
