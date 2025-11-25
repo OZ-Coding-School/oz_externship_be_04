13da338d5e1b9d8ff8282dcf4a25f32afe5b495f
s4ngmin-9 <s4ngmin9@naver.com>
2025-11-25 19:01:09 +0900

    💡 chore: 모델명 복수형 -> 단수형으로 변경
    - Users -> User
    - StudyGroupBaseModel -> StudyGroup

--- apps/users/models.py
+++ apps/users/models.py
@@ -9,7 +9,7 @@ class GenderChoices(models.TextChoices):
     FEMALE = "F", "여성"
 
 
-class Users(TimeStampedModel, AbstractBaseUser):
+class User(TimeStampedModel, AbstractBaseUser):
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=30)
     nickname = models.CharField(max_length=10, unique=True)
