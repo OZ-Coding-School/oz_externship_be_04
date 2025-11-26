d698c0ede2e5a2c26bc1169d1a81fa84e0a0a9e1
daebagi <daeburm0115@naver.com>
2025-11-26 14:28:44 +0900

    🐛 fix: category 모델 하나의 파일로 병합 및 related_name 추가, 순환참조 해결을 위한 참조 문자열로 변환

--- apps/lectures/models/category.py
+++ apps/lectures/models/category.py
@@ -12,3 +12,37 @@ class Category(TimeStampedModel):
 
     def __str__(self) -> str:
         return self.name
+
+
+class LectureCategory(TimeStampedModel):
+    pk = models.CompositePrimaryKey("lecture_id", "category_id")
+    lecture = models.ForeignKey(
+        "lectures.CrawledLecture", on_delete=models.CASCADE, related_name="lecture_categories", help_text="강의 id"
+    )
+    category = models.ForeignKey(
+        "lectures.Category", on_delete=models.CASCADE, related_name="category_lectures", help_text="카테고리 id"
+    )
+
+    class Meta:
+        ordering = ["pk"]
+        db_table = "lecture_categories"
+
+    def __str__(self) -> str:
+        return f"[{self.lecture}] {self.category}"
+
+
+class UserPreferCategory(TimeStampedModel):
+    pk = models.CompositePrimaryKey("user_id", "category_id")
+    user = models.ForeignKey(
+        "users.User", on_delete=models.CASCADE, related_name="preferred_categories", help_text="유저 id"
+    )
+    category = models.ForeignKey(
+        "lectures.Category", on_delete=models.CASCADE, related_name="preferred_by_users", help_text="카테고리 id"
+    )
+
+    class Meta:
+        ordering = ["pk"]
+        db_table = "user_prefer_categories"
+
+    def __str__(self) -> str:
+        return f"[{self.user}] {self.category}"
