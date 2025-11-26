b6c178adb21974b329c613aeebf72ad06f5f0ee0
seokhun14 <seokmomil@naver.com>
2025-11-26 15:46:23 +0900

    ✨ feat: 강의 북마크 모델 참조 추가

--- apps/lectures/models/__init__.py
+++ apps/lectures/models/__init__.py
@@ -2,10 +2,12 @@ from apps.lectures.models.category import Category, LectureCategory, UserPreferC
 from apps.lectures.models.crawled_lecture import CrawledLecture
 from apps.lectures.models.crawled_lecture_review import CrawledLectureReview
 from apps.lectures.models.lecture_bookmark import LectureBookmark
+
 __all__ = [
     "CrawledLecture",
     "CrawledLectureReview",
     "Category",
     "LectureCategory",
     "UserPreferCategory",
+    "LectureBookmark",
 ]
