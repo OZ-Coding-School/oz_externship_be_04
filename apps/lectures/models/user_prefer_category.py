472f9eca0a8276b2f3e7727393c62f0f0a6faf46
daebagi <daeburm0115@naver.com>
2025-11-26 11:25:12 +0900

    ✨ feat: user_prefer_category 모델 작성

---
+++ apps/lectures/models/user_prefer_category.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+from apps.lectures.models.category import Category
+from apps.users.models import User
+
+
+class UserPreferCategory(models.Model):
+    pk = models.CompositePrimaryKey("user_id", "category_id", help_text="복합 pk키")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, help_text="유저 id")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, help_text="카테고리 id")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ["pk"]
+        db_table = "user_prefer_categories"
+
+    def __str__(self) -> str:
+        return f"[{self.user}] {self.category}"
