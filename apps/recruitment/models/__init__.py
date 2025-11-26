4c6bd5baa01f1a64058623114328250513c60228
Yong-back <skathd6406@gmail.com>
2025-11-26 14:14:35 +0900

    🐛 fix: recruitment/models/__init__ 모델 명 추가 recruitment/models/__init__ 모델 명 추가하지않은 부분 수정하였습니다.

--- apps/recruitment/models/__init__.py
+++ apps/recruitment/models/__init__.py
@@ -1,3 +1,9 @@
 from .recruitment import Recruitment
+from .recruitment_bookmarks import RecruitmentBookmarks
+from .recruitment_search_logs import RecruitmentSearchLogs
 
-__all__ = ["Recruitment"]
+__all__ = [
+    "Recruitment",
+    "RecruitmentBookmarks",
+    "RecruitmentSearchLogs",
+]
--- apps/recruitment/models/recruitment_search_logs.py
+++ apps/recruitment/models/recruitment_search_logs.py
@@ -6,35 +6,19 @@ class RecruitmentSearchLogs(models.Model):
 
     user_id = models.ForeignKey(
         "users.User",
-        null = False,
-        on_delete = models.CASCADE,
-        db_column = "user_id",
-        related_name = "recruitment_search_logs",
-        help_text = "유저 ID"
+        null=False,
+        on_delete=models.CASCADE,
+        db_column="user_id",
+        related_name="recruitment_search_logs",
+        help_text="유저 ID",
     )
-    keyword = models.CharField(
-        max_length = 255,
-        null = False,
-        blank = False,
-        help_text = "검색어"
-    )
-    created_at = models.DateTimeField(
-        auto_now_add = True,
-        null = False,
-        help_text = "공고 검색 기록 생성일시"
-    )
-    updated_at = models.DateTimeField(
-        auto_now = True,
-        null = True,
-        help_text = "공고 검색 기록 수정일시"
-    )
+    keyword = models.CharField(max_length=255, null=False, blank=False, help_text="검색어")
+    created_at = models.DateTimeField(auto_now_add=True, null=False, help_text="공고 검색 기록 생성일시")
+    updated_at = models.DateTimeField(auto_now=True, null=True, help_text="공고 검색 기록 수정일시")
 
     class Meta:
         db_table = "recruitment_search_logs"
-        indexes = [
-            models.Index(fields = ["keyword"]),
-            models.Index(fields = ["created_at"])
-        ]
+        indexes = [models.Index(fields=["keyword"]), models.Index(fields=["created_at"])]
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"{self.user_id} - {self.keyword} - {self.created_at:%Y-%m-%d %H:%M:%S}"
