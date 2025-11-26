fd330536def7bcfc6288591be6f3244a0163776b
daebagi <daeburm0115@naver.com>
2025-11-26 17:49:48 +0900

    ✨ feat: 카테고리 시리얼라이저 작성

---
+++ apps/lectures/serializers/category_serializer.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from apps.lectures.models import Category
+
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ["id", "name", "created_at", "updated_at"]
+        read_only_fields = ["created_at", "updated_at"]
