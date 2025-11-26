a89aed67c2aca42cc8e064e27b28994d656d05c8
daebagi <daeburm0115@naver.com>
2025-11-26 11:23:53 +0900

    ✨ feat: __init__에 "Category",     "LectureCategory",     "UserPreferCategory" 추가

--- apps/lectures/models/__init__.py
+++ apps/lectures/models/__init__.py
@@ -1,7 +1,13 @@
+from apps.lectures.models.category import Category
 from apps.lectures.models.crawled_lecture import CrawledLecture
 from apps.lectures.models.crawled_lecture_review import CrawledLectureReview
+from apps.lectures.models.lecture_category import LectureCategory
+from apps.lectures.models.user_prefer_category import UserPreferCategory
 
 __all__ = [
     "CrawledLecture",
     "CrawledLectureReview",
+    "Category",
+    "LectureCategory",
+    "UserPreferCategory",
 ]
