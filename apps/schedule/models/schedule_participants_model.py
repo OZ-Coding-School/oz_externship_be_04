c11546b67a0f4fb3a7f352b19c2eeaeeac26510b
BH13KDR <bh13kdr@example.com>
2025-11-26 17:36:03 +0900

    apply TimeStampModel, add serializer, scaffold empty services

--- apps/schedule/models/schedule_participants_model.py
+++ apps/schedule/models/schedule_participants_model.py
@@ -1,26 +1,27 @@
 from django.db import models
+
 from ...core.models import TimeStampedModel
 
 
 class ScheduleParticipantsModel(TimeStampedModel):
 
     schedule = models.ForeignKey(
-        'group_schedules',
+        "group_schedules",
         on_delete=models.CASCADE,
-        related_name='participants',
-        db_column = "schedule_id",
-        help_text = '스케줄',
+        related_name="participants",
+        db_column="schedule_id",
+        help_text="스케줄",
     )
     member = models.ForeignKey(
-        'group_members',
+        "group_members",
         on_delete=models.CASCADE,
-        related_name='participants',
-        db_column = 'member_id',
-        help_text = '참가 맴버',
+        related_name="participants",
+        db_column="member_id",
+        help_text="참가 맴버",
     )
 
     class Meta:
-        db_table = 'schedule_participants'
+        db_table = "schedule_participants"
 
     def __str__(self) -> str:
-        return f'{self.schedule_id} - {self.member_id}'
+        return f"{self.schedule_id} - {self.member_id}"
---
+++ apps/schedule/serializers/group_schedule_serializer.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+from rest_framework import serializers
+
+from apps.schedule.models.group_schedule_model import GroupScheduleModel
+from apps.study_groups.models import StudyGroup
+
+
+class GroupScheduleSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = GroupScheduleModel
+        fields = "__all__"
+
+    def validate_study_group(self, value):
+        if not StudyGroup.objects.filter(study_group=value).exists():
+            raise serializers.ValidationError("스터디 그룹이 존재하지 않습니다.")
+        return value
+
+    def validate_title(self, value):
+        if len(value) < 1:
+            raise serializers.ValidationError("제목을 입력해주세요.")
+        if len(value) > 100:
+            raise serializers.ValidationError("제목은 100자를 초과할 수 없습니다.")
+
+    def validate_objective(self, value):
+        if len(value) > 500:
+            raise serializers.ValidationError("설명은 500자를 초과할 수 없습니다.")
+        return value
+
+    def validate(self, data):
+        if data["session_date"] < datetime.today().date():
+            raise serializers.ValidationError({"detail": "날자 설정이 잘못 되었습니다."})
+        if data["start_time"] >= data["end_time"]:
+            raise serializers.ValidationError({"detail": "시간 설정이 잘못 되었습니다."})
+        return data
