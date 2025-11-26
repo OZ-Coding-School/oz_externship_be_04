b69d4a15fb8daa01e4f44d1263448f09a3b94556
LuSiEDa <wltlrtmdgus@naver.com>
2025-11-26 14:41:04 +0900

    [feat/notification] notification 모델 작성 ✨ feat: notification 모델 작성, serializer등 패키지화해서 관리
    notification 모델 작성, serializer등 패키지화해서 관리

---
+++ apps/notification/models/notification.py
@@ -0,0 +1,32 @@
+from django.db import models
+
+from apps.core.models import TimeStampedModel
+
+
+class Notification(TimeStampedModel):
+    class NotificationType(models.TextChoices):
+        STUDY_JOIN = "STUDY_JOIN", "스터디 그룹에 새로운 구성원이 참가한 경우 알림"
+        STUDY_NOTE_CREATE = "STUDY_NOTE_CREATE", "스터디 구성원이 스터디 기록을 작성한 경우 알림"
+        STUDY_REVIEW_REQUEST = "STUDY_REVIEW_REQUEST", "스터디 종료가 도래한 경우 알림"
+        APPLICATION_ACCEPT = "APPLICATION_ACCEPT", "지원내역이 승인된 경우 알림"
+        APPLICATION_REJECT = "APPLICATION_REJECT", "지원내역이 거절된 경우 알림"
+        ADD_APPLICATION = "ADD_APPLICATION", "공고를 올린 스터디 그룹의 리더에게 새로운 지원내역이 있는 경우 알림"
+        TODAY_SCHEDULE = "TODAY_SCHEDULE", "금일 스케줄 알림"
+        UPCOMING_SCHEDULE = "UPCOMING_SCHEDULE", "스케줄 하루 전에 임박한 예정 스케줄 알림"
+
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE, null=False, related_name="notifications")
+    content = models.CharField(max_length=300)
+    type = models.CharField(choices=NotificationType.choices)
+    is_read = models.BooleanField(default=False)
+    back_url_link = models.CharField(max_length=500)
+
+    class Meta:
+        db_table = "notification"
+        verbose_name = "Notification"
+        verbose_name_plural = "Notifications"
+        indexes = [
+            models.Index(fields=["user", "is_read"]),
+        ]
+
+    def __str__(self) -> str:
+        return f"{self.type} : {self.user}"
