b942405f1ce38f5aadc09b347e35bd6b522660ff
Yong-back <skathd6406@gmail.com>
2025-11-26 16:54:17 +0900

    ✨feat: Recruitment 관련 모델 작성 Recruitment 관련 모델 recruitment_bookmarks , recruitment_search_logs 모델작성
    recruitment/models/__init__ 에 RecruitmentBookmarks, RecruitmentSearchLogs 추가

--- apps/recruitment/models/__init__.py
+++ apps/recruitment/models/__init__.py
@@ -1,8 +1,8 @@
 from .recruitment import Recruitment
-from .recruitment_bookmarks import RecruitmentBookmarks
-from .recruitment_search_logs import RecruitmentSearchLogs
 from .recruitment_attachment import RecruitmentAttachment
+from .recruitment_bookmarks import RecruitmentBookmarks
 from .recruitment_image import RecruitmentImage
+from .recruitment_search_logs import RecruitmentSearchLogs
 
 __all__ = [
     "Recruitment",
