5e9bf02d3282e2149c6096185fb3a0ca0bcb8d6b
TeaCat-Neya <TeaCat0416@gmail.com>
2025-11-28 02:35:10 +0900

    ✨ feat: GroupMember 모델 추가
    TimeStampedModel 사용

--- apps/study_groups/models/models.py
+++ apps/study_groups/models/models.py
@@ -1,6 +1,10 @@
 from django.db import models
 
+from apps.core.models import TimeStampedModel
+from apps.users.models.users import User
 
+
+# 스터디 그룹
 class StudyGroup(models.Model):
     class StudyGroupStatusChoices(models.TextChoices):
         PENDING = "PENDING"
@@ -26,3 +30,16 @@ class StudyGroup(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+# 그룹 멤버
+class GroupMember(TimeStampedModel):
+    study_group_id = models.ForeignKey("study_groups.StudyGroup", on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    is_leader = models.BooleanField(default=False)
+
+    class Meta:
+        db_table = "study_members"
+
+    def __str__(self) -> str:
+        return f"{self.study_group_id.name}의 멤버 {self.user_id.nickname}"
