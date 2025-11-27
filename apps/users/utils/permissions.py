51dbd531d865a0369f75a000b75e03d999a464f3
chdan-hub <ksmii358@gmail.com>
2025-11-26 18:02:08 -0800

    ✨ feat: 관리자 또는 스테프인지 권한 체크하는 코드 작성

---
+++ apps/users/utils/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework import permissions
+
+class StaffOrSuperUser(permissions.BasePermission):
+    """
+    관리자(staff or superuser) 로그인만 허용합니다.
+    """
+    def has_permission(self, request, view):
+        user = request.user
+        return bool(
+            user
+            and user.is_authenticated
+            and (user.is_staff or user.is_superuser)
+        )
