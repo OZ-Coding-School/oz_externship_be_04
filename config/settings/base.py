11b8c8bef94ef9cb0a62af56750c99d153684df5
2joonkim <joon271694@gmail.com>
2025-11-27 14:37:05 +0900

    🚑 hotfix: application 모델 및 recruitment 마이그레이션 파일 추가

--- config/settings/base.py
+++ config/settings/base.py
@@ -39,6 +39,7 @@ CUSTOM_APPS: list[str] = [
     "apps.users",
     "apps.recruitment",
     "apps.chat",
+    "apps.application",
 ]
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + CUSTOM_APPS
 
