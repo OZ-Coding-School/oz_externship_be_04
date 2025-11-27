d45a6b777ba3d37362f817ebfcbbb8ea46568a44
TeaCat-Neya <TeaCat0416@gmail.com>
2025-11-27 13:25:03 +0900

    ✨ feat: StudyLecture 모델 작성
    TimeStampedModel 사용하여 작성

---
+++ apps/study_groups/models/study_lecture.py
@@ -0,0 +1,11 @@
+from django.db import models
+from apps.core.models import TimeStampedModel
+
+
+class StudyLecture(TimeStampedModel):
+    lecture_id = models.ForeignKey("lectures.CrawledLecture", on_delete=models.CASCADE)
+    study_group_id = models.ForeignKey("study_groups.StudyGroup", on_delete=models.CASCADE)
+
+
+    class Meta:
+        db_table = "study_lectures"
