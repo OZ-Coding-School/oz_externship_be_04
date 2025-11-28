29f5c1482f39da903361beddc3bc15f1297fa0c3
kickcik <4778009@gmail.com>
2025-11-28 18:27:40 +0900

    🐛 fix: lectures url 앞에 슬래시 추가

--- apps/lectures/urls/crawled_lecture_url.py
+++ apps/lectures/urls/crawled_lecture_url.py
@@ -3,5 +3,5 @@ from django.urls import path
 from apps.lectures.views.crawled_lecture_view import CrawledLectureListAPIView
 
 urlpatterns = [
-    path("lectures", CrawledLectureListAPIView.as_view(), name="lectures"),
+    path("/lectures", CrawledLectureListAPIView.as_view(), name="lectures"),
 ]
