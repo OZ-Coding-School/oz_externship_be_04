e45d0d96ed638f303a5a3574575597d424a15867
han-son-03 <rkdgksths11@naver.com>
2025-11-28 16:05:33 +0900

    ✨ feat: notification serializer 추가
    알림 특성상 대부분 일방향 통신이므로 read_only_fields를 지정하여 정보 신뢰성을 유지
    
    NotificationReadSerializer를 만들어, 사용자가 단건 혹은 전체 알림 조회 시 is_read 상태를 안전하게 처리할 수 있도록 고려함

---
+++ apps/notification/serializers/notification_serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers
+
+from apps.notification.models import Notification
+
+
+# 일반적인 필드는 읽기 전용으로 두어 일방향으로 보게 되는 알림의 특성상 읽기만 가능한 시리얼라이저
+class NotificationSerializer(serializers.ModelSerializer["Notification"]):
+    class Meta:
+        model = Notification
+        fields = [
+            "id",
+            "user",
+            "type",
+            "content",
+            "is_read",
+            "created_at",
+            "back_url_link",
+        ]
+        read_only_fields = fields
+
+
+# is_read 시리얼라이저를 통해 유저가 "읽음"에 대해서는 post로 변경할 수 있도록 해줌
+class NotificationReadSerializer(serializers.ModelSerializer["Notification"]):
+    class Meta:
+        model = Notification
+        fields = ["is_read"]
