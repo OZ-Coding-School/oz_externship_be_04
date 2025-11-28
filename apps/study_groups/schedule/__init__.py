b85cfe680ec6d043dd6f81091f9773dde1b4be10
BH13KDR <bh13kdr@example.com>
2025-11-28 14:59:47 +0900

    study_group 안에 schedule_model을 포함시켰습니다.

---
+++ apps/study_groups/schedule/__init__.py
@@ -0,0 +1,2 @@
+from apps.study_groups.schedule.models import GroupSchedule
+from apps.study_groups.schedule.models import ScheduleParticipants
--- config/settings/base.py
+++ config/settings/base.py
@@ -41,6 +41,7 @@ CUSTOM_APPS: list[str] = [
     "apps.chat",
     "apps.application",
     "apps.notification",
+    "apps.study_groups.schedule",
 ]
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + CUSTOM_APPS
 
