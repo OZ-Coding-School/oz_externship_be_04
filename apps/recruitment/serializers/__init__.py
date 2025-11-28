2a28a440753fa4b41cc0141fc4b6db2d8e88a535
romiii35 <romiii9735@gmail.com>
2025-11-28 10:33:15 +0900

    ✨ feat : __init__.py에 TagSerializer, RecruitmentTagUpdateSerializer 추가
     __init__.py에 TagSerializer, RecruitmentTagUpdateSerializer 추가

--- apps/recruitment/serializers/__init__.py
+++ apps/recruitment/serializers/__init__.py
@@ -1,5 +1,8 @@
 from .recruitment_image import RecruitmentImageSerializer
+from .tags import RecruitmentTagUpdateSerializer, TagSerializer
 
 __all__ = [
     "RecruitmentImageSerializer",
+    "TagSerializer",
+    "RecruitmentTagUpdateSerializer",
 ]
