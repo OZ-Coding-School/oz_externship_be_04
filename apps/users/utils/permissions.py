1de3680570d90fccacebb606e30664c15e9c5414
chdan-hub <ksmii358@gmail.com>
2025-11-27 22:36:04 -0800

    🐛 fix: View import 삭제 후 typing.Any로 변경

--- apps/users/utils/permissions.py
+++ apps/users/utils/permissions.py
@@ -1,4 +1,5 @@
-from django.views import View
+from typing import Any
+
 from rest_framework import permissions
 from rest_framework.request import Request
 
@@ -8,7 +9,7 @@ class StaffOrSuperUser(permissions.BasePermission):
     관리자(staff or superuser) 로그인만 허용합니다.
     """
 
-    def has_permission(self, request: Request, view: View) -> bool:
+    def has_permission(self, request: Request, view: Any) -> bool:
         user = request.user
 
         if not user.is_authenticated:
