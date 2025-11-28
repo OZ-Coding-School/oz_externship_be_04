2374ff9ddd768d89d93d2091a898373433e45056
TeaCat-Neya <TeaCat0416@gmail.com>
2025-11-28 11:48:44 +0900

    ✨ feat: note 앱 추가
    start app
    apps 수정
    setting에 추가

--- config/settings/base.py
+++ config/settings/base.py
@@ -42,6 +42,7 @@ CUSTOM_APPS: list[str] = [
     "apps.application",
     "apps.notification",
     "apps.schedule",
+    "apps.note",
 ]
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + CUSTOM_APPS
 
