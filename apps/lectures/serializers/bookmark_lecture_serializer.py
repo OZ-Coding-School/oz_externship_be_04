abf86750ee5a882f86f12462b1657bb6831f1969
seokhun14 <seokmomil@naver.com>
2025-12-01 13:18:17 +0900

    🐛 fix: NestedSerializer 사용하게 변경

---
+++ apps/lectures/serializers/bookmark_lecture_serializer.py
@@ -0,0 +1,28 @@
+
+from rest_framework import serializers
+
+from apps.lectures.models import CrawledLecture
+
+
+class BookmarkLectureSerializer(serializers.ModelSerializer[CrawledLecture]):
+
+    discounted_price = serializers.IntegerField(
+        source="discount_price",
+        read_only=True,
+    )
+
+    class Meta:
+        model = CrawledLecture
+        fields = [
+            "id",
+            "title",
+            "instructor",
+            "total_class_time",
+            "original_price",
+            "discounted_price",
+            "difficulty",
+            "thumbnail_img_url",
+            "platform",
+            "url_link",
+        ]
+        read_only_fields = fields
--- apps/lectures/serializers/lecture_bookmark_serializer.py
+++ apps/lectures/serializers/lecture_bookmark_serializer.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
-
 from apps.lectures.models import LectureBookmark
+from apps.lectures.serializers.bookmark_lecture_serializer import BookmarkLectureSerializer
+
 
 
 class LectureBookmarkSerializer(serializers.ModelSerializer[LectureBookmark]):
@@ -12,28 +13,11 @@ class LectureBookmarkSerializer(serializers.ModelSerializer[LectureBookmark]):
 
 
 class LectureBookmarkListSerializer(serializers.ModelSerializer[LectureBookmark]):
-    id = serializers.IntegerField(source="lecture.id", read_only=True)
-    title = serializers.CharField(source="lecture.title", read_only=True)
-    instructor = serializers.CharField(source="lecture.instructor", read_only=True)
-    total_class_time = serializers.IntegerField(source="lecture.total_class_time", read_only=True)
-    original_price = serializers.IntegerField(source="lecture.original_price", read_only=True)
-    discounted_price = serializers.IntegerField(source="lecture.discount_price", read_only=True)
-    difficulty = serializers.CharField(source="lecture.difficulty", read_only=True)
-    thumbnail_img_url = serializers.CharField(source="lecture.thumbnail_img_url", read_only=True)
-    platform = serializers.CharField(source="lecture.platform", read_only=True)
-    url_link = serializers.CharField(source="lecture.url_link", read_only=True)
-
     class Meta:
         model = LectureBookmark
-        fields = [
-            "id",
-            "title",
-            "instructor",
-            "total_class_time",
-            "original_price",
-            "discounted_price",
-            "difficulty",
-            "thumbnail_img_url",
-            "platform",
-            "url_link",
-        ]
+        fields = ["lecture"]
+
+    def to_representation(self, instance):
+        lecture_data = BookmarkLectureSerializer(instance.lecture).data
+        return lecture_data
+
