4ea924a23d1b9e02400228331e1b324f9157954a
LuSiEDa <wltlrtmdgus@naver.com>
2025-12-01 17:28:13 +0900

    text_fix

--- apps/notification/tests/test_notification.py
+++ apps/notification/tests/test_notification.py
@@ -1,13 +1,34 @@
 from typing import Any
-from django.test import TestCase, Client
+
+from django.test import Client, TestCase
 from django.urls import reverse
 
+from apps.notification.models import Notification
+from apps.notification.serializers.notification_serializers import (
+    NotificationSerializer,
+)
+from apps.users.models import User
+
+
 class NotificationTests(TestCase):
-    def setUp(self):
+    def setUp(self) -> None:
         self.client = Client()
 
-    def test_mock_notification(self) -> None:
+    def test_notification(self) -> None:
         url = reverse("notification:notification-list")
         resp: Any = self.client.get(url)
         assert resp.resolver_match.view_name == "notification:notification-list"
         assert resp.status_code == 200
+
+
+class NotificationSerializerTests(TestCase):
+    def setUp(self) -> None:
+        self.user: User = User.objects.create(email="test@example.com", password="password123")
+
+    def test_serializer_valid_data(self) -> None:
+        notification: Notification = Notification.objects.create(
+            user=self.user, content="test", type=Notification.NotificationType.STUDY_JOIN, back_url_link="/test/url"
+        )
+        data = NotificationSerializer(notification).data
+
+        assert data["type"] == notification.type
