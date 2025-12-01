a483f750c08f6f34e74e9904d93286100653c575
summmer5 <jisun131200@gmail.com>
2025-12-01 13:45:46 +0900

    ✨ feat: Serializer에 타입 파라미터 추가

--- apps/users/serializers/oauth_serializers.py
+++ apps/users/serializers/oauth_serializers.py
@@ -1,9 +1,19 @@
 from typing import Any, Dict
 
+from rest_framework import serializers
 
-class SocialLoginSerializer(serializers.Serializer):
+
+class SocialLoginSerializer(serializers.Serializer[Dict[str, Any]]):
     provider = serializers.CharField()
     provider_id = serializers.CharField()
+    nickname = serializers.CharField(required=False, allow_blank=True, allow_null=True)
+    profile_image = serializers.URLField(required=False, allow_blank=True, allow_null=True)
 
     def validate(self, attrs: Dict[str, Any]) -> Dict[str, Any]:
-        return super().validate(attrs)
+        provider = attrs.get("provider")
+        provider_id = attrs.get("provider_id")
+
+        if not provider or not provider_id:
+            raise serializers.ValidationError("필수 필드 누락")
+
+        return attrs
