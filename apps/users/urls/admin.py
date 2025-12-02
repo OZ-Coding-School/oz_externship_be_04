8bf47d033c21320f41d5153498c18fab7543115a
chdan-hub <ksmii358@gmail.com>
2025-12-01 23:50:24 -0800

    ✨ feat: 유저 정보 상세 조회 url 매핑
    # Conflicts:
    #	config/urls.py
    
    # Conflicts:
    #	config/urls.py

--- apps/users/urls/admin.py
+++ apps/users/urls/admin.py
@@ -1,11 +1,19 @@
 from django.urls import path
 
-from apps.users.views.admin_account_spec import AdminAccountListSpec
+from apps.users.views.admin_account_spec import (
+    AdminAccountDetailSpec,
+    AdminAccountListSpec,
+)
 
 urlpatterns = [
     path(
-        "accounts",
+        "api/v1/admin/account",
         AdminAccountListSpec.as_view(),
         name="admin_account_list",
+    ),
+    path(
+        "api/v1/admin/account/<int:account_id>",
+        AdminAccountDetailSpec.as_view(),
+        name="admin_account_detail",
     ),
 ]
