036b3d9d220b975d230a20ac554bcb42e57b9e94
kang9805 <zzionkk@naver.com>
2025-12-02 23:28:27 +0900

    ♻️ refactor : applicant_urls 수정
    urls 매핑 뒤에 name으로 별칭 부여

--- apps/application/urls/applicant_urls.py
+++ apps/application/urls/applicant_urls.py
@@ -9,8 +9,10 @@ from apps.application.views.applicant_application_views import (
 
 urlpatterns = [
     # Applicant
-    path("recruitments/<str:recruitment_uuid>/applications", ApplicationCreateView.as_view()),
-    path("applications/mine", MyApplicationListView.as_view()),
-    path("applications/<str:application_uuid>", MyApplicationDetailView.as_view()),
-    path("applications/<str:application_uuid>/cancel", ApplicationCancelView.as_view()),
+    path(
+        "recruitments/<str:recruitment_uuid>/applications", ApplicationCreateView.as_view(), name="application-create"
+    ),
+    path("applications/mine", MyApplicationListView.as_view(), name="application-list-mine"),
+    path("applications/<str:application_uuid>", MyApplicationDetailView.as_view(), name="application-detail"),
+    path("applications/<str:application_uuid>/cancel", ApplicationCancelView.as_view(), name="application-cancel"),
 ]
