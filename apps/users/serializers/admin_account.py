ca031a172318da65493745030bc353000613ed67
chdan-hub <ksmii358@gmail.com>
2025-12-01 22:46:02 -0800

    🐛 fix: 어드민 회원 정보 상세 조회용 시리얼라이저 수정

--- apps/users/serializers/admin_account.py
+++ apps/users/serializers/admin_account.py
@@ -4,6 +4,9 @@ from apps.users.models import User
 
 
 class AdminAccountSerializer(serializers.ModelSerializer[User]):
+    """
+    어드민 회원 목록 조회용 serializer
+    """
     role = serializers.SerializerMethodField()
     status = serializers.SerializerMethodField()
     created_at = serializers.DateTimeField(read_only=True)
@@ -39,15 +42,42 @@ class AdminAccountSerializer(serializers.ModelSerializer[User]):
             else:
                 return "inactive"
 
-class AdminAccountDetailSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    email = serializers.EmailField()
-    nickname = serializers.CharField()
-    name = serializers.CharField()
-    phone_number = serializers.CharField()
-    birthday = serializers.DateField(format="%Y-%m-%d")
-    gender = serializers.ChoiceField(choices=("M", "F"))
-    status = serializers.ChoiceField(choices=("active", "inactive", "withdrew"))
-    role = serializers.ChoiceField(choices=("user", "staff", "admin"))
-    profile_img_url = serializers.URLField()
-    created_at = serializers.DateTimeField(format="%Y-%m-%dT%H:%M:%S.%f%z")
+class AdminAccountDetailSerializer(serializers.ModelSerializer[User]):
+    """
+    어드민 회원 정보 상세 조회용 serializer
+    """
+    role = serializers.SerializerMethodField()
+    status = serializers.SerializerMethodField()
+    created_at = serializers.DateTimeField(read_only=True)
+
+    class Meta:
+        model = User
+        fields = [
+            "id",
+            "email",
+            "nickname",
+            "name",
+            "phone_number",
+            "birthday",
+            "gender",
+            "status",
+            "role",
+            "profile_img_url",
+            "created_at",
+        ]
+
+        def get_role(self, obj: User) -> str:
+            if obj.is_superuser:
+                return "admin"
+            if obj.is_staff:
+                return "staff"
+            return "user"
+
+        def get_status(self, obj: User) -> str:
+            if obj.is_active:
+                return "active"
+            else:
+                if obj.withdrawals.exists():
+                    return "withdrew"
+                else:
+                    return "inactive"
