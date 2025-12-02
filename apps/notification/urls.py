7fe6ba4596243a54a6cd1bc4f69d25da5efa8093
han-son-03 <rkdgksths11@naver.com>
2025-12-02 11:11:17 +0900

    🐛 fix      : rebase 이후 urls.py 구조 통일
    urls.py의 파일 위치를 rebase하며 원격 브랜치와 통일했습니다(기존: apps/notification/urls/api_urls.py 레퍼런스 참고 구조-> apps/notification/urls.py)
    
    최종 Push 직전 mypy cache 문제로 다른 파트에서 오류가 있었습니다.
    
    최종 Push 직전 다른 파트에서 mypy에 의한 cache 문제가 있었습니다

--- apps/notification/urls.py
+++ apps/notification/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 
 from apps.notification.views.notification_views import NotificationListAPIView
+from apps.notification.views.read_views import (
+    NotificationReadAllViews,
+    NotificationReadView,
+)
 
 app_name = "notification"
 
 urlpatterns = [
     path("", NotificationListAPIView.as_view(), name="notification-list"),
+    path("<int:notification_id>/read", NotificationReadView.as_view(), name="notification-read"),
+    path("read-all", NotificationReadAllViews.as_view(), name="notification-read-all"),
 ]
