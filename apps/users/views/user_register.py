5627913dad691073ce8585534848d1b4086d81f9
dothebest9 <smrmtjsrn11@naver.com>
2025-12-03 16:14:31 +0900

    🐛 fix: 회원가입view수정

--- apps/users/views/user_register.py
+++ apps/users/views/user_register.py
@@ -1,45 +1,10 @@
-from rest_framework.request import Request
-from rest_framework.response import Response
-from rest_framework.views import APIView, status
-from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.tokens import Token
+from django.contrib.auth import get_user_model
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.generics import CreateAPIView
+from rest_framework.permissions import IsAuthenticated, AllowAny
+from apps.users.serializers.user_serializer import UserSerializer
 
-from apps.users.serializers.user_register_serializers import UserLoginSerializer, UserRegisterSerializer
-
-
-class UserRegisterAPIView(APIView):
-    def post(self, request: Request):
-        serializer = UserRegisterSerializer(data=request.data)
-        if serializer.is_valid():
-            user = serializer.save()
-            token: Token = TokenObtainPairSerializer.get_token(user)
-            res = Response(
-                {
-                    "user": serializer.data,
-                    "message": "register successs",
-                    "token": {
-                        "access": str(token.access_token),
-                        "refresh": str(token),
-                    },
-                },
-                status=status.HTTP_200_OK,
-            )
-            return res
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-
-class UserLoginAPIView(APIView):
-    def post(self, request: Request):
-        token_serializer = TokenObtainPairSerializer(data=request.data)
-        if token_serializer.is_valid():
-            user = token_serializer.user
-            serializer = UserLoginSerializer(user)
-            return Response(
-                {
-                    "user": serializer.data,
-                    "message": "login success",
-                    "token": token_serializer.validated_data,
-                },
-                status=status.HTTP_200_OK,
-            )
-        return Response(token_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class SignupView(CreateAPIView):
+    model = get_user_model()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
