1e55db73dc619461390fe9c0a60fcf0a832b8dd5
dothebest9 <smrmtjsrn11@naver.com>
2025-12-03 17:19:10 +0900

    🐛 fix: isort,black,mypy 가동후 다시 업로드

--- apps/users/urls/user_signup_url.py
+++ apps/users/urls/user_signup_url.py
@@ -1,4 +1,5 @@
 from django.urls import path
+
 from apps.users.views.user_register import SignupView
 
 urlpatterns = [
--- apps/users/views/user_register.py
+++ apps/users/views/user_register.py
@@ -1,8 +1,10 @@
 from django.contrib.auth import get_user_model
-from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+from rest_framework.authentication import BasicAuthentication, SessionAuthentication
 from rest_framework.generics import CreateAPIView
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
 from apps.users.serializers.user_serializer import UserSerializer
+
 
 class SignupView(CreateAPIView):
     model = get_user_model()
