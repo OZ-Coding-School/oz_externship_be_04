514fab3bc1f24f97391efb36c23cdbc2bef5e1d1
dothebest9 <smrmtjsrn11@naver.com>
2025-12-03 16:04:48 +0900

    ✨ feat: 사용자회원가입기능 추가

--- apps/users/models/users.py
+++ apps/users/models/users.py
@@ -1,6 +1,7 @@
 from typing import Optional, Any
 
 from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.models import PermissionsMixin
 from django.db import models
 
 from apps.core.models import TimeStampedModel
@@ -14,18 +15,18 @@ class UserManager(BaseUserManager["User"]):
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
-        user.save(using=self._db)
+        user.save()
         return user
 
     def create_superuser(self, email:str, password:str|None =None, **extra_fields:Any)->"User":
-        return self.create_user(
-            email,
-            password=password,
-            is_staff=True,
-            is_superuser=True,
-            is_active=True,
-            **extra_fields,
-        )
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(('Superuser must have is_superuser=True.'))
+        return self.create_user(email, password, **extra_fields)
 
 
 class GenderChoices(models.TextChoices):
@@ -33,19 +34,23 @@ class GenderChoices(models.TextChoices):
     FEMALE = "F", "여성"
 
 
-class User(TimeStampedModel, AbstractBaseUser):
+class User(TimeStampedModel, PermissionsMixin, AbstractBaseUser):
     email = models.EmailField(unique=True)
     name = models.CharField(max_length=30)
     nickname = models.CharField(max_length=10, unique=True)
     phone_number = models.CharField(max_length=20, unique=True)
     gender = models.CharField(max_length=1, choices=GenderChoices.choices)
-    birthday = models.DateField()
+    birthday = models.DateField(null=True)
     profile_img_url = models.URLField(max_length=255)
     is_active = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
     USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+    objects = UserManager()
 
+    def __str__(self)-> str:
+        return self.email
     _status_value: Optional[str] = None
 
     @property
