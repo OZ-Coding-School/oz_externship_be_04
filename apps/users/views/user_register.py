949a48d8effd65155faf4430079b1fd3233257cc
dothebest9 <smrmtjsrn11@naver.com>
2025-12-04 10:43:00 +0900

    🐛 fix      : CreateApiView -> APIView로 수정

--- apps/users/views/user_register.py
+++ apps/users/views/user_register.py
@@ -1,12 +1,18 @@
-from django.contrib.auth import get_user_model
-from rest_framework.authentication import BasicAuthentication, SessionAuthentication
-from rest_framework.generics import CreateAPIView
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework import status
+from rest_framework.permissions import AllowAny
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from apps.users.serializers.user_serializer import UserSerializer
 
 
-class SignupView(CreateAPIView):
-    model = get_user_model()
-    serializer_class = UserSerializer
-    permission_classes = [AllowAny]
+class SignupView(APIView):
+    permission_classes = (AllowAny,)
+
+    def post(self, request: Request) -> Response:
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
