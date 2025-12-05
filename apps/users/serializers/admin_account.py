b31922225655280bdc80584c9c5cff64789b1138
chdan-hub <ksmii358@gmail.com>
2025-12-05 01:35:57 -0800

    🐛 fix: black, isort, mypy로 인한 수정

--- apps/users/serializers/admin_account.py
+++ apps/users/serializers/admin_account.py
@@ -1,3 +1,6 @@
+from datetime import datetime
+from typing import Any, Optional
+
 from rest_framework import serializers
 
 from apps.users.models import User
@@ -27,7 +30,7 @@ class AdminAccountSerializer(serializers.ModelSerializer[User]):
             "created_at",
         ]
 
-    def get_withdraw_at(self, obj: User):
+    def get_withdraw_at(self, obj: User) -> Optional[datetime]:
         withdrawal = obj.withdrawals.order_by("-created_at").first()
         if withdrawal is None:
             return None
@@ -61,10 +64,12 @@ class AdminAccountDetailSerializer(serializers.ModelSerializer[User]):
             "profile_img_url",
         ]
 
-class AdminAccountUpdateSerializer(serializers.Serializer):
+
+class AdminAccountUpdateSerializer(serializers.Serializer[Any]):
     """
     어드민 페이지 회원 정보 수정 요청용 Serializer
     """
+
     nickname = serializers.CharField(
         required=False,
         max_length=10,
@@ -78,24 +83,14 @@ class AdminAccountUpdateSerializer(serializers.Serializer):
     phone_number = serializers.RegexField(
         regex=r"^\d{11}$",
         required=False,
-        error_messages={"invalid" : "11자리 숫자로 구성해야 합니다."},
+        error_messages={"invalid": "11자리 숫자로 구성해야 합니다."},
         help_text="휴대폰 번호(예: 01012349876)",
     )
-    birthday = serializers.DateTimeField(
-        required=False,
-        help_text="생년월일 (예: 2001-09-07)"
-    )
-    gender = serializers.ChoiceField(
-        required=False,
-        choices=[("M", "M"), ("F", "F")],
-        help_text="성별 (M/F 선택)"
-    )
+    birthday = serializers.DateTimeField(required=False, help_text="생년월일 (예: 2001-09-07)")
+    gender = serializers.ChoiceField(required=False, choices=[("M", "M"), ("F", "F")], help_text="성별 (M/F 선택)")
     status = serializers.ChoiceField(
         required=False,
         choices=[("active", "active"), ("inactive", "inactive"), ("withdrew", "withdrew")],
-        help_text="회원 상태 (active/inactive/withdrew)"
+        help_text="회원 상태 (active/inactive/withdrew)",
     )
-    profile_img_url = serializers.URLField(
-        required=False,
-        help_text="프로필 이미지 URL"
-    )
+    profile_img_url = serializers.URLField(required=False, help_text="프로필 이미지 URL")
