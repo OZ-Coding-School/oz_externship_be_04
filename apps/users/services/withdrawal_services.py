7eba523a8c6ea29ccf94f527c59ccb6e413c90d8
UNGBI78 <mybluewings78@gmail.com>
2025-12-05 18:48:09 +0900

    🐛 [fix/withdraw] CKPS-34 회원 탈퇴 service 로직 else -> atomic 교체 ( 안정성 )

--- apps/users/services/withdrawal_services.py
+++ apps/users/services/withdrawal_services.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+from django.db import transaction
 from rest_framework.exceptions import ValidationError
 
 from apps.users.models import User, Withdrawal
@@ -8,7 +9,7 @@ from apps.users.models import User, Withdrawal
 def withdraw_service(user: User, withdrawal_data: dict[str, Any]) -> Withdrawal:
     if not user.is_active:
         raise ValidationError("이미 탈퇴 처리된 유저 입니다. 다시 로그인 하시면 계정 복구를 진행하실 수 있습니다.")
-    else:
+    with transaction.atomic():
         withdrawal = Withdrawal.objects.create(user=user, **withdrawal_data)
         user.is_active = False
         user.save()
