a32ddefd406aab801f8aab18434383a61bb53b59
romiii35 <romiii9735@gmail.com>
2025-12-08 17:28:40 +0900

    💡 chore : url/init.py 수정(tags 추가)
    url/init.py 수정(tags 추가)

--- apps/recruitment/urls/__init__.py
+++ apps/recruitment/urls/__init__.py
@@ -1,3 +1,4 @@
 from .recruitment_bookmarks import urlpatterns as bookmarks_patterns
+from .tags import urlpatterns as tags_patterns
 
-urlpatterns = bookmarks_patterns
+urlpatterns = bookmarks_patterns + tags_patterns
