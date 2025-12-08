0bae71abe1adda9fadf90f75b81967257a7cdf38
summmer5 <jisun131200@gmail.com>
2025-12-08 11:03:59 +0900

    🎨 style: black, isort, mypy 코드 포매터 실행

--- apps/users/services/oauth_services.py
+++ apps/users/services/oauth_services.py
@@ -17,9 +17,7 @@ class SocialLoginService:
             raise ValueError("유효하지 않은 소셜 로그인 제공자입니다.")
 
         social_user: SocialUser | None = (
-            SocialUser.objects.filter(provider=provider, provider_id=provider_id)
-            .select_related("user")
-            .first()
+            SocialUser.objects.filter(provider=provider, provider_id=provider_id).select_related("user").first()
         )
 
         if social_user:
