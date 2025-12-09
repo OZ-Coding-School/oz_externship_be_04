dd24b92950791868e83ad8aa9d494b6afae93dcd
s4ngmin-9 <s4ngmin9@naver.com>
2025-12-09 17:35:36 +0900

    ♻️ refactor: 경로 뒤 슬래시 제거
    경로 마지막에 있던 / 제거했습니다

--- apps/chat/urls/v1.py
+++ apps/chat/urls/v1.py
@@ -12,45 +12,45 @@ from apps.chat.views.message_view import (
 )
 
 urlpatterns = [
-    # 채팅방 목록 조회
+    # 채팅방 목록 조회 (GET)
     path(
-        "chatrooms/",
+        "chatrooms",
         ChatRoomListView.as_view(),
         name="api_v1_chatrooms_list",
     ),
-    # 채팅방 상세 조회
+    # 채팅방 상세조회 (GET)
     path(
-        "chatrooms/<int:group_id>/",
+        "chatrooms/<int:group_id>",
         ChatRoomDetailView.as_view(),
         name="api_v1_chatrooms_retrieve",
     ),
-    # 채팅방 읽음 처리
+    # 전체 읽음 처리 (POST)
     path(
-        "chatrooms/<int:group_id>/read/",
+        "chatrooms/<int:group_id>/read",
         ChatRoomMarkAllReadView.as_view(),
         name="api_v1_chatrooms_mark_all_read",
     ),
-    # 멤버별 읽음 처리 API
+    # 멤버별 읽음 처리 (POST)
     path(
-        "chatroom/<int:group_id>/members/<int:member_id>/read/",
+        "chatrooms/<int:group_id>/members/<int:member_id>/read",
         ChatRoomMarkAllReadView.as_view(),
         name="api_v1_chatrooms_mark_all_read_v2",
     ),
-    # 메시지 목록 조회
+    # 메시지 목록 조회 (GET)
     path(
-        "chatrooms/<int:group_id>/messages/",
+        "chatrooms/<int:group_id>/messages",
         ChatRoomMessageListView.as_view(),
         name="api_v1_chatroom_message_list",
     ),
-    # 메시지 생성
+    # 메시지 생성 (POST)
     path(
-        "chatrooms/<int:group_id>/messages/create/",
+        "chatrooms/<int:group_id>/messages/create",
         MessageCreateView.as_view(),
         name="api_v1_chatroom_message_create",
     ),
-    # 메시지 단건 조회
+    # 메시지 단건 조회 (GET)
     path(
-        "messages/<int:message_id>/",
+        "messages/<int:message_id>",
         MessageDetailView.as_view(),
         name="api_v1_messages_retrieve",
     ),
