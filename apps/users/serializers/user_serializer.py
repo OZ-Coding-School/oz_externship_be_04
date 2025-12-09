6b1f4eedbdb2e67212b0a2eae5507b278249dc9a
dothebest9 <smrmtjsrn11@naver.com>
2025-12-09 10:41:46 +0900

    🐛 fix: user_serializer.py전화번호 양식검증추가

--- apps/users/serializers/user_serializer.py
+++ apps/users/serializers/user_serializer.py
@@ -1,3 +1,4 @@
+import re
 from typing import Any
 
 from django.contrib.auth import get_user_model
@@ -38,10 +39,23 @@ class UserSerializer(serializers.ModelSerializer[Any]):
         return value
 
     def validate_phone_number(self, value: str) -> str:
+        REGEX_PHONE_NUMBER = r"^01[0-9]-\d{3,4}-\d{4}$"
+        REGEX_PHONE_NUMBER_WITHOUT_HYPEN = r"^01[0-9]\d{8}$"
+
+        is_valid_with_hyphen = re.match(REGEX_PHONE_NUMBER, value)
+        is_valid_without_hyphen = re.match(REGEX_PHONE_NUMBER_WITHOUT_HYPEN, value)
+
+        if not (is_valid_without_hyphen or is_valid_with_hyphen):
+            raise serializers.ValidationError("전호번호 양식이 맞지않습니다.")
+        replace_number = value.replace("-", "")
         if self.instance is None:
-            if User.objects.filter(phone_number=value).exists():
+            if User.objects.filter(phone_number=replace_number).exists():
                 raise serializers.ValidationError("이미 사용 중인 전화번호입니다.")
-        return value
+        # if len(value) != 11:
+        #    raise serializers.ValidationError("전화번호 길이가 맞지않습니다")
+        # if not value.isdigit():
+        #     raise serializers.ValidationError("전화번호에 숫자만 들어가야합니다.")
+        return replace_number
 
     def create(self, validated_data: dict[str, Any]) -> Any:
         password = validated_data.pop("password")
