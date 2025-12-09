cecdd2b5e070c2ab9411fc12e4c0ccbbd704ab48
dothebest9 <smrmtjsrn11@naver.com>
2025-12-09 10:44:05 +0900

    ✅ test: 전화번호 양식검증test추가 및 변경된url경로변경

--- apps/users/tests/test_auth.py
+++ apps/users/tests/test_auth.py
@@ -9,7 +9,7 @@ User = get_user_model()
 class test_user_register(APITestCase):
     def setUp(self) -> None:
         self.client = APIClient()
-        self.signup_url = reverse("signup")
+        self.account_urls = reverse("signup")
         self.valid_user_data = {
             "email": "test@test.com",
             "password": "test_register123",
@@ -28,7 +28,7 @@ class test_user_register(APITestCase):
     def test_success_register(self) -> None:
         before = User.objects.count()
 
-        response = self.client.post(self.signup_url, self.valid_user_data)
+        response = self.client.post(self.account_urls, self.valid_user_data)
         after = User.objects.count()
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
@@ -44,14 +44,14 @@ class test_user_register(APITestCase):
                 invalid_data = self.valid_user_data.copy()
                 removed_value = invalid_data.pop(field)
 
-                response = self.client.post(self.signup_url, invalid_data)
+                response = self.client.post(self.account_urls, invalid_data)
 
                 self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
                 self.assertIn(field, response.data)
 
     def test_email(self) -> None:
-        response = self.client.post(self.signup_url, self.valid_user_data)
-        second_response = self.client.post(self.signup_url, self.valid_user_data)
+        response = self.client.post(self.account_urls, self.valid_user_data)
+        second_response = self.client.post(self.account_urls, self.valid_user_data)
 
         self.assertEqual(second_response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("email", second_response.data)
@@ -66,7 +66,7 @@ class test_user_register(APITestCase):
             gender="M",
             birthday="1990-01-01",
         )
-        response = self.client.post(self.signup_url, self.valid_user_data)
+        response = self.client.post(self.account_urls, self.valid_user_data)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("nickname", response.data)
@@ -81,7 +81,47 @@ class test_user_register(APITestCase):
             gender="M",
             birthday="1990-01-01",
         )
-        response = self.client.post(self.signup_url, self.valid_user_data)
+        response = self.client.post(self.account_urls, self.valid_user_data)
 
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
         self.assertIn("phone_number", response.data)
+
+    def test_invalid_phone_number(self) -> None:
+        invaild_phone_number = [
+            "1234567890",  # 앞자리 누락
+            "010 1234 5678",  # 공백
+            "0101234567",  # 10자리
+            "010123456789",  # 12자리
+            "abc12345678",  # 문자 포함
+            "02012345678",  # 020
+            "010-abcd-5678",  # 하이픈 문자포함
+            "010--1234-5678",  # 하이픈 중복
+        ]
+        for phone_number in invaild_phone_number:
+            with self.subTest(phone_number=phone_number):
+                valid_data = self.valid_user_data.copy()
+                valid_data["phone_number"] = phone_number
+                response = self.client.post(self.account_urls, valid_data)
+
+                self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+                self.assertIn("phone_number", response.data)
+
+    def test_valid_phone_number(self) -> None:
+        valid_phone_number = [
+            ("01023456789", "user1", "test1@test.com"),  # 하이픈 없음
+            ("010-1934-5678", "user2", "test2@test.com"),  # 하이픈 포함
+            ("01112345678", "user3", "test3@test.com"),  # 011 하이픈 없음
+            ("011-1234-5679", "user4", "test4@test.com"),  # 011 하이픈 포함
+        ]
+
+        for phone_number, nickname, email in valid_phone_number:
+            with self.subTest(phone_number=phone_number):
+                valid_data = self.valid_user_data.copy()
+                valid_data["phone_number"] = phone_number
+                valid_data["nickname"] = nickname
+                valid_data["email"] = email
+
+                response = self.client.post(self.account_urls, valid_data, format="json")
+
+                self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+                self.assertIn("phone_number", response.data)
