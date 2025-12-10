51519e37f2f345a4a6b35b8779021acd1fcc6380
BH13KDR <bh13kdr@example.com>
2025-12-10 15:31:14 +0900

    # Conflicts: #	apps/study_groups/urls.py
    # Conflicts:
    #	apps/study_groups/tests/tests.py
    #	apps/study_groups/urls.py
    
    # Conflicts:
    #	apps/study_groups/urls.py

--- apps/study_groups/urls.py
+++ apps/study_groups/urls.py
@@ -11,6 +11,10 @@ from .views.study_groups_view import (
     StudyGroupListCreateAPIView,
     StudyGroupRetrieveUpdateDestroyAPIView,
 )
+from .views.schedule_views import (
+    ScheduleDetailView,
+    ScheduleView,
+)
 from .views.study_note import (
     StudyNoteDetailView,
     StudyNoteListCreateView,
@@ -34,6 +38,10 @@ urlpatterns = [
         "/study-groups/<int:group_id>/reviews/<int:review_id>",
         StudyGroupReviewUpdateAPIView.as_view(),
         name="study-group-review-update",
+    ),
+    path("/study-groups/<int:group_id>/schedules", ScheduleView.as_view(), name="schedule"),
+    path(
+        "/study-groups/<int:group_id>/schedules/<int:schedule_id>", ScheduleDetailView.as_view(), name="schedule-detail"
     ),
     path("/study-groups/<int:group_id>/notes", StudyNoteListCreateView.as_view(), name="study-note-list-create"),
     path("/study-groups/<int:group_id>/notes/<int:note_id>", StudyNoteDetailView.as_view(), name="study-note-detail"),
