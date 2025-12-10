528265437bc76f817d478cf6e1980778c5d00bfe
UNGBI78 <mybluewings78@gmail.com>
2025-12-10 14:37:50 +0900

    💡 [chore/withdraw] 탈퇴 서비스 에러 타입 명시

--- apps/users/services/withdrawal_services.py
+++ apps/users/services/withdrawal_services.py
@@ -8,7 +8,9 @@ from apps.users.models import User, Withdrawal
 
 def withdraw_service(user: User, withdrawal_data: dict[str, Any]) -> Withdrawal:
     if not user.is_active:
-        raise ValidationError("이미 탈퇴 처리된 유저 입니다. 다시 로그인 하시면 계정 복구를 진행하실 수 있습니다.")
+        raise ValidationError(
+            {"non_field_errors": "이미 탈퇴 처리된 유저 입니다. 다시 로그인 하시면 계정 복구를 진행하실 수 있습니다."}
+        )
     with transaction.atomic():
         withdrawal = Withdrawal.objects.create(user=user, **withdrawal_data)
         user.is_active = False
