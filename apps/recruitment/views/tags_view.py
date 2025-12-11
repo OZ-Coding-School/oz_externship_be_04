f063d7f3fdb80f69d777cb0454156bf85d25863a
romiii35 <romiii9735@gmail.com>
2025-12-11 15:30:04 +0900

    💡 chore : tags 태그 및 response 응답값 수정
    tags 태그 및 response 응답값 수정

--- apps/recruitment/views/tags_view.py
+++ apps/recruitment/views/tags_view.py
@@ -1,4 +1,4 @@
-from drf_spectacular.utils import OpenApiParameter, extend_schema
+from drf_spectacular.utils import OpenApiParameter, OpenApiResponse, extend_schema
 from rest_framework import permissions, status
 from rest_framework.exceptions import ValidationError
 from rest_framework.pagination import PageNumberPagination
@@ -19,7 +19,7 @@ class TagListAPIView(APIView):
     permission_classes = [permissions.AllowAny]
 
     @extend_schema(
-        tags=["tags"],
+        tags=["Recruitment"],
         summary="태그 검색 및 조회하는 API 입니다.",
         description=(
             "태그명을 일부 검색하거나 완전 일치하는 항목을 확인할수 있습니다.\n\n"
@@ -33,7 +33,12 @@ class TagListAPIView(APIView):
         ],
         responses={
             200: TagSerializer(many=True),
-            404: {"error_detail": "요청한 페이지를 찾을 수 없습니다."},
+            404: OpenApiResponse(
+                response={
+                    "type": "object",
+                    "properties": {"error_detail": {"type": "string", "example": "요청한 페이지를 찾을 수 없습니다."}},
+                }
+            ),
         },
     )
     def get(self, request: Request) -> Response:
@@ -53,14 +58,31 @@ class TagListAPIView(APIView):
         return paginator.get_paginated_response(serializer.data)
 
     @extend_schema(
-        tags=["tags"],
+        tags=["Recruitment"],
         summary="태그 생성 API 입니다.",
         description=("존재하지 않을 경우, 태그를 생성 할수 있습니다."),
         request=TagSerializer,
         responses={
-            201: {"detail": "태그가 정상적으로 등록되었습니다."},
-            409: {"error_detail": "이미 존재하는 태그입니다."},
-            401: {"error_detail": "자격 인증 데이터가 제공되지 않았습니다."},
+            201: OpenApiResponse(
+                response={
+                    "type": "object",
+                    "properties": {"detail": {"type": "string", "example": "태그가 정상적으로 등록되었습니다."}},
+                }
+            ),
+            401: OpenApiResponse(
+                response={
+                    "type": "object",
+                    "properties": {
+                        "error_detail": {"type": "string", "example": "자격 인증 데이터가 제공되지 않았습니다."}
+                    },
+                }
+            ),
+            409: OpenApiResponse(
+                response={
+                    "type": "object",
+                    "properties": {"error_detail": {"type": "string", "example": "이미 존재하는 태그입니다."}},
+                }
+            ),
         },
     )
     def post(self, request: Request) -> Response:
