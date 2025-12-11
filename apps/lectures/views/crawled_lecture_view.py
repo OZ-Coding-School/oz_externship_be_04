966d91531828b9e4281464c1171899b36bb84faf
kickcik <4778009@gmail.com>
2025-12-11 14:38:38 +0900

    ✨ feat: mock data 처리 삭제

--- apps/lectures/views/crawled_lecture_view.py
+++ apps/lectures/views/crawled_lecture_view.py
@@ -112,34 +112,10 @@ class CrawledLectureListAPIView(APIView):
         },
     )
     def get(self, request: Request) -> Response:
-        import random
-        from decimal import Decimal
-
-        mock_data = [
-            CrawledLecture(
-                id=random.randint(1, 1000),
-                external_id=random.randint(1, 1000),
-                title=f"제목{i}",
-                instructor=f"강사{i}",
-                average_rating=Decimal(random.randint(100, 500) / 100),
-                total_class_time=random.randint(1, 60),
-                difficulty=random.choice([difficulty[0] for difficulty in CrawledLecture.DifficultyEnum.choices]),
-                platform=random.choice([platform[0] for platform in CrawledLecture.PlatformEnum.choices]),
-                original_price=random.randint(1, 1000),
-                discount_price=random.randint(1, 1000),
-                url_link=f"https://example_url_{i}.com",
-                thumbnail_img_url=f"https://example_thumbnail_{i}.com",
-            )
-            for i in range(15)
-        ]
         paginator = self.pagination_class()
-        page: list[CrawledLecture] | None
 
-        if settings.DEBUG:
-            page = paginator.paginate_queryset(mock_data, request)  # type: ignore[arg-type]
-        else:
-            queryset = self.get_queryset()
-            page = paginator.paginate_queryset(queryset, request)
+        queryset = self.get_queryset()
+        page: list[CrawledLecture] | None = paginator.paginate_queryset(queryset, request)
 
         serializer = self.serializer_class(page, many=True)
         return paginator.get_paginated_response(serializer.data)
