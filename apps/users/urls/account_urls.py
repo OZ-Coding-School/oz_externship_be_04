b99b972ca7244e0fc6fd917424e2667d7390e01b
UNGBI78 <mybluewings78@gmail.com>
2025-12-12 12:27:43 +0900

    ✨ [feat/mypage] CKPS-41 비밀번호 중복 검사 url 엔드포인트 추가

--- apps/users/urls/account_urls.py
+++ apps/users/urls/account_urls.py
@@ -1,10 +1,15 @@
 from django.urls import path
 
-from apps.users.views.user_account_views import NicknameCheckView, UserAccountView
+from apps.users.views.user_account_views import (
+    NicknameCheckView,
+    UserAccountView,
+    UserPasswordResetView,
+)
 from apps.users.views.user_register import SignupView
 
 urlpatterns = [
     path("accounts/me", UserAccountView.as_view(), name="account"),
     path("accounts/signup", SignupView.as_view(), name="signup"),
     path("accounts/check-nickname", NicknameCheckView.as_view(), name="check-nickname"),
+    path("accounts/find-password", UserPasswordResetView.as_view(), name="find-password"),
 ]
