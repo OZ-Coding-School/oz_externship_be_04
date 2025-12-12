1e86b89c95efc6cbe967902184f3c64496297b86
UNGBI78 <mybluewings78@gmail.com>
2025-12-12 11:58:49 +0900

    ✨ [feat/mypage] CKPS-41 닉네임 중복 검사 엔드포인트 추가

--- apps/users/urls/account_urls.py
+++ apps/users/urls/account_urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from apps.users.views.user_account_views import UserAccountView
+from apps.users.views.user_account_views import NicknameCheckView, UserAccountView
 from apps.users.views.user_register import SignupView
 
 urlpatterns = [
     path("accounts/me", UserAccountView.as_view(), name="account"),
     path("accounts/signup", SignupView.as_view(), name="signup"),
+    path("accounts/check-nickname", NicknameCheckView.as_view(), name="check-nickname"),
 ]
