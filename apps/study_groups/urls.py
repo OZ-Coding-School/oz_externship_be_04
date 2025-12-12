0ed72abcdca1667bffc67c2cdb0bb2624c1f3eef
TeaCat-Neya <TeaCat0416@gmail.com>
2025-12-12 11:54:40 +0900

    🎨 style: code_formatting

--- apps/study_groups/urls.py
+++ apps/study_groups/urls.py
@@ -18,6 +18,10 @@ urlpatterns = [
     path("/study-groups/<int:group_id>", StudyGroupUpdateAPIView.as_view(), name="study-group-update"),
     path("/study-groups/<int:group_id>", StudyGroupDestroyAPIView.as_view(), name="study-group-delete"),
     path("/study-groups/<int:group_id>/delegate-leader", DelegateLeaderAPIView.as_view(), name="delegate-leader"),
-    path("/study-groups/<int:group_id>/members/<int:member_id>", KickStudyGroupMemberAPIView.as_view(), name="study-group-kick"),
+    path(
+        "/study-groups/<int:group_id>/members/<int:member_id>",
+        KickStudyGroupMemberAPIView.as_view(),
+        name="study-group-kick",
+    ),
     path("/study-groups/<int:group_id>/members/me", LeaveStudyGroupMeAPIView.as_view(), name="study-group-leave"),
 ]
