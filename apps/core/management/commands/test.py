b6a71013f753d16fbff2395dc7e166982d7d0fef
codfin02 <codfin02@gmail.com>
2025-12-12 18:47:59 +0900

    테스트 명령에서 Redis 없을 때 건너뛰기

--- apps/core/management/commands/test.py
+++ apps/core/management/commands/test.py
@@ -7,11 +7,14 @@ from django.core.management.commands.test import Command as TestCommand
 
 class Command(TestCommand):
     def handle(self, *test_labels: Any, **options: Any) -> None:
-
         self.teardown_redis()
         super().handle(*test_labels, **options)
         self.teardown_redis()
 
     @staticmethod
     def teardown_redis() -> None:
-        redis.client.Redis.from_url(f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}/15").flushdb()
+        try:
+            redis.client.Redis.from_url(f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}/15").flushdb()
+        except Exception:
+            # 로컬 테스트에서 레디스가 없어도 넘어가게 한다.
+            pass
