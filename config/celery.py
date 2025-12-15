f05531e76886e23ce57b501b7470656d1108bd92
Jun <dlwnsehd13@gmail.com>
2025-12-15 13:04:32 +0900

    fix: beat 충돌 방지용.
    2. 변경 또는 추가사항을 아래에 간략하게 작성하세요 ( 필수 )
    settings에서 beat 충돌 날 수 있어서 넣었습니다.

--- config/celery.py
+++ config/celery.py
@@ -6,18 +6,20 @@ from celery import Celery
 from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
-
-os.environ["DISABLE_GSSAPI"] = "true"  # 윈도우라서 넣었어요
+os.environ["DISABLE_GSSAPI"] = "true"
 
 app = Celery("config")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
-
 app.autodiscover_tasks()
 
-app.conf.beat_schedule = {
-    "sync_inflearn_every_midnight": {
-        "task": "lectures.sync_inflearn_task",
-        "schedule": crontab(hour=0, minute=0),
-    },
-}
+app.conf.timezone = "Asia/Seoul"
+
+app.conf.beat_schedule.update(
+    {
+        "sync_inflearn_every_midnight": {
+            "task": "lectures.sync_inflearn_task",
+            "schedule": crontab(hour=0, minute=0),
+        },
+    }
+)  # beat 충돌 방지
