70262819dd31aba7aee7ab626c5c4a3c77b0f3d0
codfin02 <codfin02@gmail.com>
2025-12-15 17:17:22 +0900

    스터디그룹 목록 401 케이스 추가

--- apps/study_groups/tests/test_study_group_api.py
+++ apps/study_groups/tests/test_study_group_api.py
@@ -55,6 +55,10 @@ class StudyGroupAPITest(APITestCase):
         self.assertEqual(len(response.data), 1)
         self.assertEqual(response.data[0]["name"], "ongoing")
 
+    def test_list_study_groups_unauthenticated(self) -> None:
+        response = self.client.get(self.base_url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
     def test_retrieve_study_group(self) -> None:
         group = StudyGroup.objects.create(**self._make_payload("detail"))
 
