08cbb0c5ece19a86cd85f233f0ce302f9db0d3b3
dothebest9 <smrmtjsrn11@naver.com>
2025-12-15 10:31:56 +0900

    🐛 fix: account_urls.py 로그인 엔드포인트 추가

--- apps/users/urls/account_urls.py
+++ apps/users/urls/account_urls.py
@@ -5,6 +5,7 @@ from apps.users.views.user_account_views import (
     UserAccountView,
     UserPasswordResetView,
 )
+from apps.users.views.user_login_view import LoginView
 from apps.users.views.user_register import SignupView
 
 urlpatterns = [
@@ -12,4 +13,5 @@ urlpatterns = [
     path("accounts/signup", SignupView.as_view(), name="signup"),
     path("accounts/check-nickname", NicknameCheckView.as_view(), name="check-nickname"),
     path("accounts/find-password", UserPasswordResetView.as_view(), name="find-password"),
+    path("accounts/login", LoginView.as_view(), name="login"),
 ]
