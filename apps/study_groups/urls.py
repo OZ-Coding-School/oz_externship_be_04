c601a9ac5ab744a9561d4cd698bbc7e61b546690
codfin02 <codfin02@gmail.com>
2025-12-16 11:50:15 +0900

    pr 피드백 반영

--- apps/study_groups/urls.py
+++ apps/study_groups/urls.py
@@ -1,32 +1,10 @@
 from django.urls import path
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-from .views.review_view import (
-    StudyGroupReviewCreateAPIView,
-    StudyGroupReviewUpdateAPIView,
-)
-
-=======
-from .views import StudyNoteAPIView, StudyNoteDetailAPIView
-
-<<<<<<< HEAD
->>>>>>> 52d1f99 (노트 작성/목록 API와 테스트 추가)
-=======
-from .views.review_view import (
-    StudyGroupReviewCreateAPIView,
-    StudyGroupReviewUpdateAPIView,
-)
-
->>>>>>> bfdf286 (pr 피드백 반영)
-=======
 from .views import StudyNoteAPIView, StudyNoteDetailAPIView
 from .views.review_view import (
     StudyGroupReviewCreateAPIView,
     StudyGroupReviewUpdateAPIView,
 )
-
->>>>>>> 9a16835 (urls 복구)
 from .views.study_groups_view import (
     DelegateLeaderAPIView,
     KickStudyGroupMemberAPIView,
@@ -41,9 +19,6 @@ from .views.study_groups_view import (
 urlpatterns = [
     path("/study-groups", StudyGroupListAPIView.as_view(), name="study-group-list"),
     path("/study-groups/create", StudyGroupCreateAPIView.as_view(), name="study-group-create"),
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
     path("/study-groups/<int:group_id>", StudyGroupRetrieveAPIView.as_view(), name="study-group-detail"),
     path("/study-groups/<int:group_id>/update", StudyGroupUpdateAPIView.as_view(), name="study-group-update"),
     path("/study-groups/<int:group_id>/delete", StudyGroupDestroyAPIView.as_view(), name="study-group-delete"),
@@ -54,57 +29,13 @@ urlpatterns = [
         name="study-group-kick",
     ),
     path("/study-groups/<int:group_id>/members/me", LeaveStudyGroupMeAPIView.as_view(), name="study-group-leave"),
->>>>>>> 9a16835 (urls 복구)
     path("/study-groups/<int:group_id>/reviews", StudyGroupReviewCreateAPIView.as_view(), name="study-group-review"),
     path(
         "/study-groups/<int:group_id>/reviews/<int:review_id>",
         StudyGroupReviewUpdateAPIView.as_view(),
         name="study-group-review-update",
     ),
-<<<<<<< HEAD
-    path("/study-groups", StudyGroupCreateAPIView.as_view(), name="study-group-create"),
-    path("/study-groups/<int:group_id>", StudyGroupRetrieveAPIView.as_view(), name="study-group-detail"),
-    path("/study-groups/<int:group_id>", StudyGroupUpdateAPIView.as_view(), name="study-group-update"),
-    path("/study-groups/<int:group_id>", StudyGroupDestroyAPIView.as_view(), name="study-group-delete"),
-    path("/study-groups/<int:group_id>/delegate-leader", DelegateLeaderAPIView.as_view(), name="delegate-leader"),
-=======
-    path("/study-groups/<int:pk>", StudyGroupRetrieveAPIView.as_view(), name="study-group-detail"),
-    path("/study-groups/<int:pk>/update", StudyGroupUpdateAPIView.as_view(), name="study-group-update"),
-    path("/study-groups/<int:pk>/delete", StudyGroupDestroyAPIView.as_view(), name="study-group-delete"),
-    path(
-        "/study-groups/<int:group_id>/delegate-leader",
-        DelegateLeaderAPIView.as_view(),
-        name="delegate-leader",
-    ),
->>>>>>> 52d1f99 (노트 작성/목록 API와 테스트 추가)
-    path(
-        "/study-groups/<int:group_id>/reviews",
-        StudyGroupReviewCreateAPIView.as_view(),
-        name="study-group-review",
-    ),
-    path(
-        "/study-groups/<int:group_id>/reviews/<int:review_id>",
-        StudyGroupReviewUpdateAPIView.as_view(),
-        name="study-group-review-update",
-    ),
-    path(
-        "/study-groups/<int:group_id>/members/<int:member_id>",
-        KickStudyGroupMemberAPIView.as_view(),
-        name="study-group-kick",
-    ),
-    path(
-        "/study-groups/<int:group_id>/members/me",
-        LeaveStudyGroupMeAPIView.as_view(),
-        name="study-group-leave",
-    ),
-    path(
-        "/study-groups/<int:study_group_id>/notes",
-        StudyNoteAPIView.as_view(),
-        name="study-note",
-    ),
-=======
     path("/study-groups/<int:study_group_id>/notes", StudyNoteAPIView.as_view(), name="study-note"),
->>>>>>> 9a16835 (urls 복구)
     path(
         "/study-groups/<int:study_group_id>/notes/<int:note_id>",
         StudyNoteDetailAPIView.as_view(),
