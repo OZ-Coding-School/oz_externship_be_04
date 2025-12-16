56f0c1269e4cfe8352090d87fb064e531dce9e99
LBH\leebh <leebh0806@naver.com>
2025-12-16 16:38:56 +0900

    ♻️ Refactor feat/chat_conumers 코드 피드백 수정
    feat/chat_consumers 피드백 수정
    
    # Conflicts:
    #	poetry.lock
    #	pyproject.toml

--- apps/chat/routing.py
+++ apps/chat/routing.py
@@ -3,5 +3,5 @@ from django.urls import re_path
 from .consumers.chat_consumer import ChatConsumer
 
 websocket_urlpatterns = [
-    re_path(r"^wss/chatrooms/(?P<group_id>[0-9a-f-]+)/?$", ChatConsumer.as_asgi()),
+    re_path(r"^wss/chatrooms/(?P<group_id>\d+)/?$", ChatConsumer.as_asgi()),
 ]
--- config/asgi.py
+++ config/asgi.py
@@ -9,8 +9,25 @@ https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 
 import os
 
+from channels.auth import AuthMiddlewareStack # type: ignore
+from channels.routing import (  # type: ignore[import-untyped]
+    ProtocolTypeRouter,
+    URLRouter,
+)
 from django.core.asgi import get_asgi_application
+
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+
+from apps.chat.routing import websocket_urlpatterns
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AuthMiddlewareStack(
+            URLRouter(websocket_urlpatterns)
+        ),
+    }
+)
