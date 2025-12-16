d2a129aa05213f36df791fde348f5840b6ffc957
daebagi <daeburm0115@naver.com>
2025-12-16 11:35:29 +0900

    🐛 fix: category fields에서 created_at, updated_at 삭제

--- apps/lectures/serializers/category_serializer.py
+++ apps/lectures/serializers/category_serializer.py
@@ -6,5 +6,5 @@ from apps.lectures.models import Category
 class CategorySerializer(serializers.ModelSerializer[Category]):
     class Meta:
         model = Category
-        fields = ["id", "name", "created_at", "updated_at"]
-        read_only_fields = ["id", "created_at", "updated_at"]
+        fields = ["id", "name"]
+        read_only_fields = ["id"]
