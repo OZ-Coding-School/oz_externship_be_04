2193414f3be850d4c330c9e01d5efeda3a355d4c
romiii35 <romiii9735@gmail.com>
2025-12-16 11:16:21 +0900

    💡 chore : recruitment admin. serializer-lecture 부분 수정
    recruitment admin. serializer-lecture 부분 수정

--- apps/recruitment/serializers/recruitment_admin_serializer.py
+++ apps/recruitment/serializers/recruitment_admin_serializer.py
@@ -31,11 +31,12 @@ class AdminRecruitmentTagSerializer(serializers.ModelSerializer[Tag]):
         read_only_fields = fields
 
 
-class AdminRecruitmentLectureSerializer(serializers.ModelSerializer[CrawledLecture]):
-    class Meta:
-        model = CrawledLecture
-        fields = ["id", "title", "instructor", "thumbnail_img_url", "url_link"]
-        read_only_fields = ["id", "title", "instructor", "thumbnail_img_url", "url_link"]
+class AdminRecruitmentLectureSerializer(serializers.Serializer):
+    id = serializers.IntegerField()
+    title = serializers.CharField()
+    instructor = serializers.CharField()
+    thumbnail_img_url = serializers.CharField()
+    url_link = serializers.CharField()
 
 
 class AdminRecruitmentAttachmentSerializer(serializers.ModelSerializer[RecruitmentAttachment]):
