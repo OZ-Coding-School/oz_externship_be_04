da8e26c7f6d474dcbc6fd4051cabcaa0e865fda2
moxopills <moxopills@gmail.com>
2025-12-17 10:48:19 +0900

    ♻️ refactor : s3 리펙토링

--- apps/core/S3.py
+++ apps/core/S3.py
@@ -124,10 +124,7 @@ class S3Uploader:
             S3FileValidator.validate_content_type(content_type)
             S3FileValidator.validate_mime_match(ext, content_type)
 
-            if file_name and "." in file_name:
-                key = f"{prefix}{uuid.uuid4()}_{file_name}"
-            else:
-                key = f"{prefix}{uuid.uuid4()}_{file_name}.{ext}"
+            key = f"{prefix}{uuid.uuid4()}_{file_name}"
 
             presigned_post = cls.get_s3_client().generate_presigned_post(
                 Bucket=cls.get_bucket_name(),
@@ -220,6 +217,5 @@ class S3Uploader:
             "key": key,
             "headers": {"Content-Type": content_type},
         }
-
 
 s3_uploader = S3Uploader()
