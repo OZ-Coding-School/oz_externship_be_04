14026340b933c10f41a1a50e6f7dc97d9b3621cd
TeaCat-Neya <TeaCat0416@gmail.com>
2025-12-17 13:45:12 +0900

    🐛 fix: 일부 패턴 병합 및 name 수정
    restful하게 관리하기 위해 도메인 일치하는 메소드들 정리 및 주석 추가

--- apps/study_groups/urls.py
+++ apps/study_groups/urls.py
@@ -8,26 +8,15 @@ from .views.study_groups_view import (
     DelegateLeaderAPIView,
     KickStudyGroupMemberAPIView,
     LeaveStudyGroupMeAPIView,
-    StudyGroupCreateAPIView,
-    StudyGroupDestroyAPIView,
-    StudyGroupListAPIView,
-    StudyGroupRetrieveAPIView,
-    StudyGroupUpdateAPIView,
+    StudyGroupListCreateAPIView,
+    StudyGroupRetrieveUpdateDestroyAPIView,
 )
 
 urlpatterns = [
-    path("/study-groups", StudyGroupListAPIView.as_view(), name="study-group-list"),
-    path("/study-groups/create", StudyGroupCreateAPIView.as_view(), name="study-group-create"),
-    path("/study-groups/<int:group_id>/reviews", StudyGroupReviewCreateAPIView.as_view(), name="study-group-review"),
-    path(
-        "/study-groups/<int:group_id>/reviews/<int:review_id>",
-        StudyGroupReviewUpdateAPIView.as_view(),
-        name="study-group-review-update",
-    ),
-    path("/study-groups", StudyGroupCreateAPIView.as_view(), name="study-group-create"),
-    path("/study-groups/<int:group_id>", StudyGroupRetrieveAPIView.as_view(), name="study-group-detail"),
-    path("/study-groups/<int:group_id>", StudyGroupUpdateAPIView.as_view(), name="study-group-update"),
-    path("/study-groups/<int:group_id>", StudyGroupDestroyAPIView.as_view(), name="study-group-delete"),
+    # StudyGroup
+    path("/study-groups", StudyGroupListCreateAPIView.as_view(), name="study-group-list-create"),
+    path("/study-groups/<int:group_id>", StudyGroupRetrieveUpdateDestroyAPIView.as_view(), name="study-group-rud"),
+    # GroupMember 관리
     path("/study-groups/<int:group_id>/delegate-leader", DelegateLeaderAPIView.as_view(), name="delegate-leader"),
     path(
         "/study-groups/<int:group_id>/members/<int:member_id>",
@@ -35,4 +24,11 @@ urlpatterns = [
         name="study-group-kick",
     ),
     path("/study-groups/<int:group_id>/members/me", LeaveStudyGroupMeAPIView.as_view(), name="study-group-leave"),
+    # Review
+    path("/study-groups/<int:group_id>/reviews", StudyGroupReviewCreateAPIView.as_view(), name="study-group-review"),
+    path(
+        "/study-groups/<int:group_id>/reviews/<int:review_id>",
+        StudyGroupReviewUpdateAPIView.as_view(),
+        name="study-group-review-update",
+    ),
 ]
