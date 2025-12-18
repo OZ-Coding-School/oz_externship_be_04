8fc135236530235267ed64c8ac131c4311d74eb6
dothebest9 <smrmtjsrn11@naver.com>
2025-12-18 17:30:58 +0900

    🐛 fix: account_urls.py 에 find password 발송, 인증 추가
    # Conflicts:
    #	apps/users/urls/account_urls.py

--- apps/users/urls/account_urls.py
+++ apps/users/urls/account_urls.py
@@ -7,6 +7,12 @@ from apps.users.views.oauth_views import (
     NaverCallBackView,
     NaverLoginView,
 )
+from apps.users.views.email_auth_views import (
+    EmailSignUpVerifyView,
+    EmailSignUpView,
+    FindPasswordSendEmailView,
+    FindPasswordVerifyEmailView,
+)
 from apps.users.views.token_views import LogoutView, TokenRefreshView
 from apps.users.views.user_account_views import (
     NicknameCheckView,
@@ -42,4 +48,8 @@ urlpatterns = [
     path("accounts/find-email/send-sms", FindEmailSendSMSView.as_view(), name="find-email-send-sms"),
     path("accounts/change-phone/send-sms", ChangePhoneSendSMSView.as_view(), name="change-phone-send-sms"),
     path("accounts/change-phone", ChangePhoneView.as_view(), name="change-phone"),
+    path("accounts/find-password/send-email", FindPasswordSendEmailView.as_view(), name="find-password-send-email"),
+    path(
+        "accounts/find-password/verify-email", FindPasswordVerifyEmailView.as_view(), name="find-password-verify-email"
+    ),
 ]
