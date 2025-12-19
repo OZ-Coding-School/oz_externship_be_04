a60b859424a1e52ae8f2deef6b123d9827e37cd2
chdan-hub <ksmii358@gmail.com>
2025-12-18 22:16:13 -0800

    🐛 fix: 오타 수정 및 파일명 변경

--- apps/users/serializers/admin_withdrawals_analytics_serializers.py
+++ apps/users/serializers/admin_withdrawals_analytics_serializers.py
@@ -16,3 +16,16 @@ class AdminWithdrawalTrendSerializer(serializers.Serializer[Any]):
     to_date = serializers.DateField()
     total = serializers.IntegerField()
     items = AdminWithdrawalTrendItemSerializer(many=True)
+
+class WithdrawalReasonPercentageItemSerializer(serializers.Serializer[Any]):
+    reason = serializers.CharField()
+    reason_label = serializers.CharField()
+    count = serializers.IntegerField()
+    percentage = serializers.FloatField()
+
+
+class WithdrawalReasonPercentageSerializer(serializers.Serializer[Any]):
+    from_date = serializers.DateField()
+    to_date = serializers.DateField()
+    total = serializers.IntegerField()
+    items = WithdrawalReasonPercentageItemSerializer(many=True)
