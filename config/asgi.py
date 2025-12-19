c1e15c830d7fca9dd6da0565793b34208d748da8
SWWS97 <parkmon26@gmail.com>
2025-12-19 20:34:25 +0900

    🐛 fix : asgi.py import 순서변경

--- config/asgi.py
+++ config/asgi.py
@@ -1,28 +1,21 @@
-"""
-ASGI config for config project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
-"""
-
 import os
 
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")
+
+django_asgi_app = get_asgi_application()
+
+# 초기화 이후에 라이브러리 및 라우팅 임포트
 from channels.routing import (  # type: ignore[import-untyped]
     ProtocolTypeRouter,
     URLRouter,
 )
-from django.core.asgi import get_asgi_application
-from django_channels_jwt_auth_middleware.auth import (  # type: ignore[import-untyped]
-    JWTAuthMiddlewareStack,
-)
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.local")  # 로컬 테스트용 .local 삭제
-
-django_asgi_app = get_asgi_application()
 
 from apps.chat.routing import websocket_urlpatterns
+
+from django_channels_jwt_auth_middleware.auth import JWTAuthMiddlewareStack  # type: ignore[import-untyped] # isort: skip
+
 
 application = ProtocolTypeRouter(
     {
