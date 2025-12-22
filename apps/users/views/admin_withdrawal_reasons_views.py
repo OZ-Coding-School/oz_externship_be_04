3100f7c0a90c76a102fdd502827e3bb7905c4d6f
chdan-hub <ksmii358@gmail.com>
2025-12-21 21:45:41 -0800

    🐛 fix: 시리얼라이저 코드 수정으로 인한 import 수정

--- apps/users/views/admin_withdrawal_reasons_views.py
+++ apps/users/views/admin_withdrawal_reasons_views.py
@@ -13,7 +13,7 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from apps.users.serializers.admin_withdrawals_analytics_serializers import (
+from apps.users.serializers.admin_analytics_serializers import (
     WithdrawalReasonMonthlyStatsSerializer,
     WithdrawalReasonPercentageSerializer,
 )
