968b2c83a98cc205ecce555fd1ee14af683e9239
dothebest9 <smrmtjsrn11@naver.com>
2025-12-24 17:42:41 +0900

    ♻️ refactor: auth_code 안쓰는 함수 제거

--- apps/users/utils/auth_code.py
+++ apps/users/utils/auth_code.py
@@ -14,11 +14,6 @@ class AuthCodeGenerator:
         auth_mail_code = base62.Base62.uuid_encode(unique_mail_code, length=length)
         return auth_mail_code
 
-    @classmethod
-    def generate_sms_code(cls) -> int:
-        auth_sms_code = random.randint(100000, 999999)
-        return auth_sms_code
-
 
 class AuthCodeCache(AuthCodeGenerator):
 
